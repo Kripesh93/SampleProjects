6fcf471ec259fbf2760452bc023319bd4275cf31
Kripesh <kripeshnavodaya@gmail.com>
2022-03-31 17:21:40 +0530

    SignalR sample

---
+++ RealTimeCharts_Server/RealTimeCharts_Server/TimerFeatures/TimerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealTimeCharts_Server.TimerFeatures
+{
+    public class TimerManager
+    {
+        private Timer timer;
+        private AutoResetEvent autoResetEvent;
+        private Action action;
+        public DateTime TimerStarted { get; }
+        public TimerManager(Action action)
+        {
+            this.action = action;
+            autoResetEvent = new AutoResetEvent(false);
+            timer = new Timer(Execute, autoResetEvent, 1000, 2000);
+            TimerStarted = DateTime.Now;
+        }
+        public void Execute(object stateInfo)
+        {
+            action();
+            if ((DateTime.Now - TimerStarted).Seconds > 60)
+            {
+                timer.Dispose();
+            }
+        }
+    }
+}
